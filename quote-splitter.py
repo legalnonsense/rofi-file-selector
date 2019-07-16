327c3a7f7ec42e9413f68c5bcb5d08ed52ef7786
Jeff Filipovits <jrfilipovits@gmail.com>
2019-07-16 12:24:27 -0400

    made quote-splitter separate the last search from the file names; next to figure out how to use the file names

--- quote-splitter.py
+++ quote-splitter.py
@@ -1,13 +1,21 @@
+#!/usr/bin/env python3
+
 import re
 import sys
+import subprocess 
 
 s=r''.join(sys.argv[1:])
 
-print(sys.argv[1:])
+strings = s.split('\'')
+last_query = strings[1].strip()
+file_names = []
 
+for s in strings:
+    if (s==last_query) or (s.strip()==''):
+        pass
+    else:
+        if (s.strip() != ''):
+            file_names.append(s)
 
-for d in s.split('\''):
-    if d:
-        print(d.strip())
-
-
+print(last_query)
+print (file_names)
