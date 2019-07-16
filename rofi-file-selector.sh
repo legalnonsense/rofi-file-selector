81c083c32195f645b03b80a3f0c1387f0d18e583
Jeff Filipovits <jrfilipovits@gmail.com>
2019-07-15 20:53:21 -0400

    now uses xdg-open-log.sh so that all files are logged

--- rofi-file-selector.sh
+++ rofi-file-selector.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 open=$((find ~/Dropbox -type f; find ~/Cases -type f; find ~/Desktop -type f; find ~/Downloads -type f) | rofi -dmenu "$@" -l 50 -i -p "Open file" -multi-select -width 2500)
-echo "${open/// \n}" | while read -r file; do xdg-open-log "$file" & done
+echo "${open/// \n}" | while read -r file; do xdg-open-log.sh "$file" & done
 #echo "${open/// \n}" | while read -r file; do xdg-open "$file"; done#!/bin/bash
 
