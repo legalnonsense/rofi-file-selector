278eb08ce80463471f5d6ecf4ded38887196fe40
Jeff Filipovits <jrfilipovits@gmail.com>
2019-07-16 16:06:26 -0400

    next step is to save the previous search, and then open ROFI by populating it

--- rofi-file-selector.sh
+++ rofi-file-selector.sh
@@ -2,7 +2,7 @@
 
 open=$((find ~/Dropbox -type f; find ~/Cases -type f; find ~/Desktop -type f; find ~/Downloads -type f) | rofi -dmenu "$@" -l 50 -i -p "Open file" -multi-select -width 2500 -format Fq)
 
-python quote-splitter.py "$open" | while read -r file; do xdg-open-log.sh "$file" & done
+python quote-splitter.py "$open" | while read -r file; do ~/Code/rofi-file-selector/xdg-open-log.sh "$file" & done
 
 
 
