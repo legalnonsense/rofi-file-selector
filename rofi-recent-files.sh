cc7878dc969a984f4d4dbe722e3c782e6c10260a
Jeff Filipovits <jrfilipovits@gmail.com>
2019-07-16 21:12:43 -0400

    doing this before making changes...

--- rofi-recent-files.sh
+++ rofi-recent-files.sh
@@ -3,4 +3,3 @@
 open=$(cat ~/Code/rofi-file-selector/xdg-open-log.log | rofi -dmenu "$@" -l 50 -i -p "Open file" -multi-select -width 2500)
 
 echo "${open/// \n}" | while read -r file; do xdg-open "$file" & done
-
