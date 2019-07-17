dec41697515a6cc0529f15640b3f617c7e2cf2ba
Jeff Filipovits <jrfilipovits@gmail.com>
2019-07-17 11:22:05 -0400

    merged xdg-open-log into rofi-file-selector.sh, so can delete xdg-open-log

--- rofi-file-selector.sh
+++ rofi-file-selector.sh
@@ -4,12 +4,18 @@
 
 open=$((find ~/Dropbox -type f; find ~/Cases -type f; find ~/Desktop -type f; find ~/Downloads -type f) | rofi -dmenu "$@" -l 50 -i -p "Open file" -multi-select -width 2500 -format Fq -filter "$(cat ~/Code/rofi-file-selector/rofi-file-selector.conf)")
 
-quote-splitter.py "$open" | while read -r file; do xdg-open-log.sh "$file" & done
+if [ -z ${open+x} ]; then
+    echo "open is unset"
+else
+    quote-splitter.py "$open" | while read -r file;
+    do
+	grep -qxF "$file" ~/Code/rofi-file-selector/xdg-open-log.log || echo "$file" >> ~/Code/rofi-file-selector/xdg-open-log.log
+	xdg-open "$file" &
+    done
+fi
 
 
 
-
-#echo "${open/// \n}" | 
 
 
 
