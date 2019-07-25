be046c1b681829819ef75303716c51e2dd49940b
Jeff Filipovits <jrfilipovits@gmail.com>
2019-07-25 14:03:00 -0400

    now handles files names which use single quotes

--- quote-splitter.py
+++ quote-splitter.py
@@ -3,23 +3,45 @@ import sys
 from os.path import expanduser
 
 s=r''.join(sys.argv[1:])
+s = s.replace(r"'\''", r"'")
 
-strings = s.split('\'')
-last_query = strings[1].strip()
+ts=s.split('\n')
+strings=[]
+
+for x in range(len(ts)):
+    for s in ts[x].split("''"):
+        strings.append(s)
+                   
+
+for x in range(len(strings)):
+
+    strings[x] = strings[x].strip()
+
+    try: 
+        if strings[x][0] == "'":
+            strings[x]=strings[x][1:]
+        if strings[x][-1] == "'":
+            strings[x]=strings[x][:-1]
+    except: pass 
+
+    #strings[x] = "'" + strings[x] + "'"
+    
+last_query = strings[0].strip()
 file_names = []
 
-for s in strings[1:]:
-    if (s.strip()==last_query) or (s.strip()==''):
+for ss in strings[1:]:
+    if (ss.strip()==last_query) or (ss.strip()==''):
         pass
     else:
-        file_names.append(s)
+        file_names.append(ss)
 
-if last_query:            
+if last_query:
     with open(expanduser("~") + "/Code/rofi-file-selector/rofi-file-selector.conf", "w") as f:
         f.write(last_query + " ")
 
 for name in file_names:
     sys.stdout.write(name)
     sys.stdout.write("\n")
+
 
 
